871b6a0817867adedc4d2acc267bb162ad202eb7
Felix692lul <felix-shumo.sun@dawsoncollege.qc.ca>
2026-09-04 11:20:06 -0400

    ...

---
+++ main(Task 1).cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+
+uint8_t value;
+
+
+void setup() {
+  DDRD &= 0x0F; // PD7-PD4 as inputs, PB3-PB0 as outputs
+  PORTD |= 0xF0;
+  DDRB &= 0X0F;
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = PIND; // read the full Port D register
+  value &= 0xF0;
+  value >>= 4;
+  value = ~value;
+  value &= 0x0F; // mask the upper nibble
+  PORTB = value; //Display result on LEDS
+  Serial.print("Binary:");
+  Serial.println(value, BIN); 
+  Serial.print("Decimal:");
+  Serial.println(value);
+  delay(1000);
+}
+
+
+
+
+
+
